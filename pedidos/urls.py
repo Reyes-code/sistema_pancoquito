c62ea8fd4b8d870986dc68dba3560ad6237264d7
Reyes-code <reyespiraligua@gmail.com>
2025-04-16 17:48:28 -0500

    Home validado

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('clientes/', views.lista_clientes, name='lista_clientes'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
+    path('', RedirectView.as_view(url='/login/', permanent=True)),
+    path('', LogoutView.as_view(next_page='login'), name='logout'), 
+
 ]
