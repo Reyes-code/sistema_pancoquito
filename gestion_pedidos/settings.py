014666085c9a27c115f2d54911edb53adb6bef7f
Reyes-code <reyespiraligua@gmail.com>
2025-08-13 23:08:27 -0500

    Settings to Railway

--- gestion_pedidos/settings.py
+++ gestion_pedidos/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,15 +81,16 @@ WSGI_APPLICATION = 'gestion_pedidos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pancoquito' ,
-        'USER': 'soter_admin',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'NAME': os.getenv('railway'),
+        'USER': os.getenv('root'),
+        'PASSWORD': os.getenv('DWGrzsNIOhCBBWNAJWZZBBPozHmunnig'),
+        'HOST': os.getenv('mysql.railway.internal'),
+        'PORT': os.getenv('3306'),
+        'OPTIONS': {
+            'ssl': {'ca': '/etc/ssl/certs/ca-certificates.crt'},
+        }
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- insertcategories.py
+++ insertcategories.py
@@ -5,41 +5,28 @@ conexion = mysql.connector.connect(
     host="localhost",
     user="soter_admin",
     password="admin123",
-    database="soter_pedidos"
+    database="pancoquito"
 )
 cursor = conexion.cursor()
 
 # Datos de categorías tech
-categorias_tech = [
-    ("Componentes PC", "Hardware"),
-    ("Periféricos", "Dispositivos"),
-    ("Accesorios PC", "Complementos"),
-    ("Cámaras", "Fotografía"),
-    ("Drones", "Tecnología Aérea"),
-    ("Redes", "Conectividad"),
-    ("Procesadores", "Hardware"),
-    ("Tarjetas Gráficas", "Hardware"),
-    ("Memorias RAM", "Hardware"),
-    ("Teclados", "Dispositivos"),
-    ("Mouse", "Dispositivos"),
-    ("Monitores", "Dispositivos"),
-    ("Coolers", "Complementos"),
-    ("Gabinetes", "Complementos"),
-    ("DSLR", "Fotografía"),
-    ("Mirrorless", "Fotografía"),
-    ("Lentes", "Fotografía"),
-    ("Drones Profesionales", "Tecnología Aérea"),
-    ("Drones Recreativos", "Tecnología Aérea"),
-    ("Routers", "Conectividad"),
-    ("Switches", "Conectividad")
+categorias = [
+    ("Pan Congelado", "Congelado"),
+    ("Hojaldres Congelados", "Congelado"),
+    ("Fritos Congelados", "Congelados"),
+    ("Panes", "Normal"),
+    ("Hojaldres", "Normal"),
+    ("Fritos", "Normal"),
+    ("Postres", "Postres"),
+    ("Galletas", "Galletas")
 ]
 
 # Insertar categorías
 query = "INSERT INTO categoria (Categoria_nombre, Categoria_Tipo) VALUES (%s, %s)"
-cursor.executemany(query, categorias_tech)
+cursor.executemany(query, categorias)
 
 conexion.commit()
 cursor.close()
 conexion.close()
 
-print(f"{len(categorias_tech)} categorías tech insertadas correctamente.")
+print(f"{len(categorias)} categorías tech insertadas correctamente.")
