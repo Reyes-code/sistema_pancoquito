3a66f1725e9ca1914e02f8c767e4e54335101515
Reyes-code <reyespiraligua@gmail.com>
2025-08-15 20:47:36 -0500

    Fix Nix build error2

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
     buildInputs = [
         pkgs.python311
-        pkgs.mysql80.dev
+        pkgs.mysql80  # Sin .dev
         pkgs.gcc
-        pkgs.zlib.dev
-        pkgs.libffi.dev
+        pkgs.zlib
+        pkgs.libffi
     ];
 }
