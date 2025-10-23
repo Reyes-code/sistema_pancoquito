bc3a56f94538397a29be59efc143636347ad81d9
Reyes-code <reyespiraligua@gmail.com>
2025-10-22 19:11:57 -0500

    Nuevo ingreso

--- pedidos/APIView.py
+++ pedidos/APIView.py
@@ -55,7 +55,7 @@ class StatsView(APIView):
             ordenes_diarias = (Pedido.objects
                 .filter(fecha_orden__date__gte=fecha_limite)
                 .values('fecha_orden__date')
-                .annotate(total=Count('orden_id'))
+                .annotate(total=Count('pedido_id'))
                 .order_by('fecha_orden__date')
             )
 
